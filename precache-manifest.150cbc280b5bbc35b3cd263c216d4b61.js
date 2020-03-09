39a461115f67d31695df5febcda29c80718827e7
hugo.ivan.encinas <hugo.ivan.encinas@accenture.com>
2020-03-09 00:05:37 -0600

    Updates

---
+++ precache-manifest.150cbc280b5bbc35b3cd263c216d4b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5f4a1cbb0ea306a85848e738d044109",
+    "url": "/trivia-game/static/media/background.e5f4a1cb.webp"
+  },
+  {
+    "revision": "4def217a7a3b6c4ff313",
+    "url": "/trivia-game/static/js/runtime~main.4def217a.js"
+  },
+  {
+    "revision": "9d1fae607bb35289cc3b",
+    "url": "/trivia-game/static/js/main.9d1fae60.chunk.js"
+  },
+  {
+    "revision": "056b3926a93ec39f57d8",
+    "url": "/trivia-game/static/js/2.056b3926.chunk.js"
+  },
+  {
+    "revision": "9d1fae607bb35289cc3b",
+    "url": "/trivia-game/static/css/main.be1874f2.chunk.css"
+  },
+  {
+    "revision": "fba75034c3888a205c4a440282cf01ff",
+    "url": "/trivia-game/index.html"
+  }
+];
